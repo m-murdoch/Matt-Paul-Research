1839572254d1d5b34519f2749bf7f21068f2b188
m-murdoch <42425165+m-murdoch@users.noreply.github.com>
2019-06-19 17:44:55 -0600

    Update Shadows.py

--- Week 5/Shadows.py
+++ Week 5/Shadows.py
@@ -1,13 +1,19 @@
 import math
 
 def shadows(A, T):
+    # we assume all points are in the sun to start
     s = [True]*len(A)
     for i in range(len(A)):
+        # shadow is end point of i's shadow
         shadow = i+math.ceil(A[i]/T)
         if shadow > len(A):
+            # avoids out of bounds errors
             shadow = len(A)
+        # examine all points within i's shadow
         for j in range(i+1, shadow):
+            # is A[j] outside the shadow
             if A[j] >= A[i]-T*(j-i):
+                # j is now the point casting shadows and becomes i
                 i = j
                 break
             else:
