8cf8fa51363b35542caf1b85c40edf081405d13d
m-murdoch <42425165+m-murdoch@users.noreply.github.com>
2019-06-24 14:40:35 -0600

    Add files via upload

---
+++ Week 5/Shadows_opt_simple.py
@@ -0,0 +1,30 @@
+import math
+
+def shadows(A, T):
+    s = [None]*len(A)
+    # examine all points in the array
+    for i in range(len(A)):
+        # examine all points potentially in the shadow of i
+        for j in range(i+1, len(A)):
+            # is j outside the shadow of i
+            if A[j] >= A[i]-T*(j-i):
+                # j is in the sun and therefore casting a new shadow. j becomes the new i
+                s[j] = True
+                i = j
+                break
+            else:
+                # j is in the shadow of i
+                s[j] = False
+    return s
+
+def main():
+    N = int(input("N: "))
+    A = [0] * N
+    for i in range(N):
+        A[i] = float(input("A(" + str(i) +"): "))
+    T = int(input("T: "))
+    print(shadows(A, T))
+    #print(shadows([1,2,3,4,1], 1))
+if __name__ == '__main__':
+   main()
+
