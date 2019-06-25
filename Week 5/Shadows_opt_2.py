ba5aa820dab05a5004c2f9271c05764e3bc2d350
m-murdoch <42425165+m-murdoch@users.noreply.github.com>
2019-06-24 18:07:51 -0600

    Update Shadows_opt_2.py

--- Week 5/Shadows_opt_2.py
+++ Week 5/Shadows_opt_2.py
@@ -1,8 +1,9 @@
 def shadows(A, T):
+    # The first point is always in the sun and therefore casting a shadow
     S = [True]
     caster = 0
-    # examine all points potentially in the cast shadow
-    for i in range(caster+1, len(A)):
+    # examine all points from 1 to N (linear)
+    for i in range(1, len(A)):
         # is i outside the shadow of the caster
         if A[i] >= A[caster]-T*(i-caster):
             # i is in the sun and therefore casting a new shadow. i becomes the new caster
