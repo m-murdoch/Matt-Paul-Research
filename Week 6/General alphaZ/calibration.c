af6b8f2da24f721efca7ab113f7b83bb781ecda8
m-murdoch <42425165+m-murdoch@users.noreply.github.com>
2019-06-29 17:57:02 -0600

    Create calibration.c

---
+++ Week 6/General alphaZ/calibration.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+
+int main(){
+	struct timeval time;
+	struct timeval time2;
+	gettimeofday(&time, NULL);
+	//printf("Testing\n");
+	gettimeofday(&time2, NULL);
+	double elapsedTime = (((double) time2.tv_sec) + ((double) time2.tv_usec)/1000000) - (((double) time.tv_sec) + ((double) time.tv_usec)/1000000);
+
+	printf("Elapsed time: %.16f seconds\n", elapsedTime); 
+}
