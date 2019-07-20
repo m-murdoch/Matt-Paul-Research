edcdf2ee0570320cae8730a85f6831501134fff2
m-murdoch <42425165+m-murdoch@users.noreply.github.com>
2019-07-20 16:45:51 -0600

    Update and rename Week 10/osp.c (triangles) to Week 10/osp/triangles/OSP.c

--- Week 10/osp/triangles/OSP.c
+++ Week 10/osp/triangles/OSP.c
@@ -1,5 +1,4 @@
-// This file is generated from test alphabets program by code generator in alphaz
-// To compile this code, use -lm option for math library.
+// triangles
 
 // Includes
 #include <stdio.h>
