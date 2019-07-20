049c3b41d6694aa23c271aea6b3d841a26340e22
m-murdoch <42425165+m-murdoch@users.noreply.github.com>
2019-07-20 16:43:20 -0600

    Update and rename Week 10/osp.c (reverse diagonals) to Week 10/reverse diagonals/osp.c

--- Week 10/reverse diagonals/osp.c
+++ Week 10/reverse diagonals/osp.c
@@ -1,5 +1,4 @@
-// This file is generated from test alphabets program by code generator in alphaz
-// To compile this code, use -lm option for math library.
+// reverse diagonals
 
 // Includes
 #include <stdio.h>
