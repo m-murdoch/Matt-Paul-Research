94d66b1dd19c310e37f83ffa9b60852005778170
m-murdoch <42425165+m-murdoch@users.noreply.github.com>
2019-07-20 16:40:59 -0600

    Update and rename Week 10/osp.c (diagonals) to Week 10/osp/diagonal/osp.c

--- Week 10/osp/diagonal/osp.c
+++ Week 10/osp/diagonal/osp.c
@@ -1,5 +1,4 @@
-// This file is generated from test alphabets program by code generator in alphaz
-// To compile this code, use -lm option for math library.
+// DIAGONAL
 
 // Includes
 #include <stdio.h>
