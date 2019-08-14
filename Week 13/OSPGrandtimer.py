d9793ffa18fb61137d7c1da0467a13ff6edf0c81
pscanlon2 <51333818+pscanlon2@users.noreply.github.com>
2019-08-14 10:55:21 -0600

    Update OSPGrandtimer.py

--- Week 13/OSPGrandtimer.py
+++ Week 13/OSPGrandtimer.py
@@ -1,53 +1,67 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+from scipy import stats
 
 if __name__ == "__main__":
     N = 23 # number of working mappings 
-    d = [[] for i in range(N)]
+    d = [[] for i in range(N)] # data
     a = ["d_-j", "d_-j_-k", "d_j_-k", "d_-k_-j", "d_k_-j", "-i_j",
     "-i_j_k","-i_k_j", "j_d_k", "j_-i_-k", "j_-k_d",
     "j_-k_i", "d_j", "d_-j_k", "d_j_k", "d_-k_j", "d_k_j", 
-    "-i_j_-k", "-i_k_-j",  "j_d_-k", "j_-i", "j_-i_k", "j_-k_-i"] # map names
+    "-i_j_-k", "-i_k_-j",  "j_d_-k", "j_-i", "j_-i_k", "j_-k_-i"] # map names (23)
 
     for i in range(N): # read in all the data
-        print(a[i])
         f = open("/Users/paullys/Desktop/research2019/OSP/mappings/" + a[i] + "/trace.csv", "r")
         r = csv.reader(f, delimiter = ",")
         for j in r:
             d[i].append(j[1])
         f.close()
-        #print(d)
-
-    d = np.array(d, dtype = float)
-    avg = [[] for i in range(N)]
-    #print("D:", d)
-
+       
+    d = np.array(d, dtype = float) # cast data as floats
+    sd = [[[0,0,0,0,0] for j in range(N)] for k in range(9)] # initalize stats table for removing outliers
+    Z = [[0 for i in range(N)] for j in range(9)] # initialize z score table
+    avg = [[] for i in range(N)] # initalize raw plotting data table
+    avg1 = [[] for i in range(N)] # initalize "outlier removed" plotting data table
+    
     for i in range(9): # 1000-5000 steps of 500 (9 steps)
         build = [0 for j in range(N)] # temp variable for average
-        sd = [[0,0,0,0,0] for j in range(N)] # temp variable for standard deviation
+        build2 = [0 for j in range(N)] # temp variable for outlier remover average
         for k in range(5): # 5 trials at each step
             for l in range(N): # 23 data sets to build averages from
                 build[l] += d[l][5*i+k]
-                sd[l][k] = d[l][5*i+k]
+                sd[i][l][k] = d[l][5*i+k]
         for l in range(N): # complete averaging
             build[l] /= 5
             avg[l].append(build[l])
-            if np.std(sd[l]) > 2: # data may not be tightly grouped if this flag goes off.
-                print("Outlier found in " , a[l] , ", problem size: " , (500*i + 1000))
+            for q in range(2): # remove a maximum of 2 outliers from each set
+                Z[i][l] = np.abs(stats.zscore(sd[i][l])) # Z scores
+                zM = np.where(Z[i][l] == max(Z[i][l]))[0][0] # max Z score
+                if max(Z[i][l]) > 1: # data may not be tightly grouped if this flag goes off
+                    print("Outlier found in " , a[l] , ", problem size: " , (500*i + 1000))
+                    sd[i][l].remove(sd[i][l][zM]) # remove the outlier
+        for l in range(N):
+            for k in range(len(sd[i][l])):
+                build2[l] += sd[i][l][k]
+            build2[l] /= len(sd[i][l])
+            avg1[l].append(build2[l])
         
     avg = np.array(avg, dtype = float)
-    #print("A: ", avg)
+    avg1 = np.array(avg, dtype = float)
 
-    colors = ["black", "tan", "chartreuse", "darkgreen", "darkcyan", "navy", "darkorchid",
-    "plum", "silver", "gold", "red", "darksalmon", "mediumspringgreen", "blue", "sandybrown", "mediumpurple", 
-    "deepskyblue", "rosybrown", "darkkhaki", "paleturquoise", "darkturquoise", "sienna", "orange"]
+    colors = ["darkkhaki", "tan", "chartreuse", "darkgreen", "darkcyan", "navy", "darkorchid",
+    "red", "silver", "gold", "black", "darksalmon", "mediumspringgreen", "grey", "sandybrown", "mediumpurple", 
+    "deepskyblue", "rosybrown", "blue", "paleturquoise", "darkturquoise", "sienna", "orange"]
     x = [1000,1500,2000,2500,3000,3500,4000,4500,5000] # the x axis
     Orders = [[] for i in range(N)]
+    Orders1 = [[] for i in range(N)]
     for i in range(N):
         Orders[i] = np.polyfit(np.log(x), np.log(avg[i]), 1)
-        print("Order " + a[i] + "  :  ")
-        print(Orders[i])
+        Orders1[i] = np.polyfit(np.log(x), np.log(avg1[i]), 1)
+        print("Order " + a[i] + "  :  ", Orders[i])
+        print("Tight Order " + a[i] + "  :  ", Orders1[i])
+        if Orders[i][0] != Orders1[i][0] or Orders[i][1] != Orders1[i][1]:
+            print("Tightening order changed result.")
         plt.plot(x, avg[i], color = colors[i], label = a[i]) # raw exponential visualization
     plt.legend()
     plt.title("Space Time Mapping Statistics")
